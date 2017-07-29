ed84c60d9a7ae43fddb3312bdc83ef7ca12dfb82
Tamara Robichet <v0ld3m0r7@gmail.com>
2017-07-29 23:29:57 +0200

    Add open in gtv text for map

--- components/Map.js
+++ components/Map.js
@@ -1,5 +1,5 @@
 export default (props) => (
-  <a href={props.linkUrl}>
+  <a className='map' href={props.linkUrl}>
     <img src={props.url} width='320' height='400' target='_blank' title={props.title} />
   </a>
 )
